4f616c5bcdd6908cd15f4766b6a901c2d2fcb52e
Tanishkagupta123 <tanishkagupta241@gmail.com>
2026-02-07 00:41:18 +0530

    first commit

--- project/settings.py
+++ project/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME":"djangodb",
+        "USER":"root",
+        "PASSWORD":"1234",
+        "HOST":"localhost",
+        "PORT":3306
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
